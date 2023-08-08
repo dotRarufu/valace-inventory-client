c16469c2b6f9349721c6fb2d6e65c7c3c7a22e88
dotRarufu <81721522+dotRarufu@users.noreply.github.com>
2023-08-08 15:14:38 +0800

    add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  manifest: {
+    theme_color: '#f63557',
+    background_color: '#35f6d4',
+    display: 'fullscreen',
+    scope: '/',
+    start_url: '/',
+    name: 'React Playground PWA',
+    short_name: 'RPWA',
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 });
